a83ca4e03f7ec79d325db7c05fcf0b9d5d47b7ae
ANKITKUMARBARIK <barikankitkumar2@gmail.com>
2025-11-04 16:14:14 +0530

    Add main.go to demonstrate closure usage in Go

---
+++ 09_closures/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func counter() func() int {
+	count := 0
+
+	return func() int {
+		count++
+		return count
+	}
+}
+
+func main() {
+
+	inc := counter()
+	fmt.Println(inc())
+	fmt.Println(inc())
+	fmt.Println(inc())
+
+}
