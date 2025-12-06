073593f80404f3089deb38458b21c981351c90c1
ANKITKUMARBARIK <barikankitkumar2@gmail.com>
2025-12-06 06:48:56 +0530

    Add PUT and DELETE request functions for Todo management in main.go

--- 24.2_crud/main.go
+++ 24.2_crud/main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 )
 
@@ -76,10 +77,79 @@ func performPostReq() {
 	fmt.Println(todoRes, res.Status)
 }
 
+func performPutReq() {
+	todo := Todo{
+		UserID:    23,
+		Id:        101,
+		Title:     "innovesta task completed",
+		Completed: true,
+	}
+	data, err := json.Marshal(todo)
+	if err != nil {
+		fmt.Println("error while updating data")
+		return
+	}
+	// fmt.Println(string(data))
+
+	// convert json data to reader-stream
+	jsonRead := bytes.NewBuffer(data)
+
+	req, errs := http.NewRequest(http.MethodPut, "https://jsonplaceholder.typicode.com/todos/1", jsonRead)
+	if errs != nil {
+		fmt.Println("error while updating data", errs)
+		return
+	}
+
+	req.Header.Set("Content-type", "application/json")
+
+	// send the request
+	client := http.Client{}
+	res, errors := client.Do(req)
+	if errors != nil {
+		fmt.Println("error while request data", errors)
+		return
+	}
+	defer res.Body.Close()
+
+	resp, errorr := io.ReadAll(res.Body)
+	if errorr != nil {
+		fmt.Println("error while responsing data", errorr)
+		return
+	}
+	fmt.Println(string(resp), res.Status)
+
+}
+
+func performDeleteReq() {
+	req, err := http.NewRequest(http.MethodDelete, "https://jsonplaceholder.typicode.com/todos/1", nil)
+	if err != nil {
+		fmt.Println("error while deleting todo", err)
+		return
+	}
+
+	req.Header.Set("Content-type", "application/json")
+
+	client := http.Client{}
+	res, errs := client.Do(req)
+	if errs != nil {
+		fmt.Println("error while sending delete request", errs)
+		return
+	}
+	defer res.Body.Close()
+
+	data, errors := io.ReadAll(res.Body)
+	if errors != nil {
+		fmt.Println("error while reading delete response", errors)
+		return
+	}
+	fmt.Println(string(data), res.Status)
+}
+
 func main() {
 	fmt.Println("Learning CRUD...")
 
 	performGetReq()
 	performPostReq()
-
+	performPutReq()
+	performDeleteReq()
 }
